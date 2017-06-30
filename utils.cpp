3626a1b49be208e5ebdcc2e94ea73406e4007ff9
Naohiro IWAMOTO <naodai24@gmail.com>
2017-06-30 12:30:57 +0900

    preEmphasis だけ WebAssembly に変換するてすと
    - 配列ではなく数値が返された
      ポインタは整数に変換される...?

--- utils.cpp
+++ utils.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include <array>
+#include <emscripten/emscripten.h>
 
-#define N 1024
+#define N 8192
 #define PI 3.14159265
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 /*
  * pre emphasis filter
  *   y(n) = x(n) - p x(n - 1)
  */
-std::array<float, N> preEmphasis(std::array<float, N> _signal, float p) {
-  std::array<float, N> y;
+EMSCRIPTEN_KEEPALIVE
+char * preEmphasis(std::array<float, N> _signal[N], float p) {
+  static char y[N];
   y[0] = _signal[0];
 
   for(int i = 0; i < N; i++) {
@@ -24,11 +29,15 @@ std::array<float, N> preEmphasis(std::array<float, N> _signal, float p) {
  * hamming window
  *   w(n) = 0.54 - 0.46 cos( 2 PI n / (M - 1) ), 0 <= n <= M - 1
  */
-std::array<float, N> hamming(std::array<float, N> _signal) {
-  std::array<float, N> w;
-  for(int i = 0; i < N; i += 1) {
-    w[i] = _signal[i] * (0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1)));
-  }
+// std::array<float, N> hamming(std::array<float, N> _signal) {
+//   std::array<float, N> w;
+//   for(int i = 0; i < N; i += 1) {
+//     w[i] = _signal[i] * (0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1)));
+//   }
+//
+//   return w;
+// }
 
-  return w;
+#ifdef __cplusplus
 }
+#endif
