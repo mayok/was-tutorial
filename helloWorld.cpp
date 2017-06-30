d7fe9b4a08551a9b7666ebd841e8fc958641a282
Naohiro IWAMOTO <naodai24@gmail.com>
2017-06-30 14:20:02 +0900

    complex (複素数) も使える！

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <iostream>
+#include <complex>
 
 int main(int argc, char ** argv) {
+  const std::complex<double> j(0.0, 1.0);
+  std::complex<double> z1(-1.0, 1.0);
+  std::complex<double> z2(2.0, -2.0);
+
   printf("Hello World\n");
+
+  std::cout << z1.real() << ", " << z1.imag() << "i" << "\n";
+  std::cout << arg(z1) << "\n";
+  std::cout << norm(z1) << "\n";
+  std::cout << conj(z1) << "\n";
 }
