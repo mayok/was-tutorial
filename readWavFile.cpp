1404ce676568f639992b38fac3cb2b0068d55659
Naohiro IWAMOTO <naodai24@gmail.com>
2017-07-02 14:32:30 +0900

    matplotlibcpp に関する行を消した

--- readWavFile.cpp
+++ readWavFile.cpp
@@ -2,14 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <complex>
+#include <map>
 #include <math.h>
-#define WITHOUT_NUMPY
-#include "matplotlibcpp.h"
 
 #define PI 4 * atan(1.0)
 #define NUM_FFT 2048
-
-namespace plt = matplotlibcpp;
 
 std::vector<std::complex<double> > fft(std::vector<std::complex<double> > x) {
   int N = x.size();
@@ -47,22 +44,17 @@ void powerSpectrum(std::vector<double> frame) {
   frame.resize(NUM_FFT);
   std::vector<std::complex<double> > fc(frame.begin(), frame.end());
 
-  std::cout << frame.size() << "\n";
   std::vector<std::complex<double> > fftc = fft(fc);
 
   // TODO: これをすると segmentation fault が起きるので，なんとかする
   // std::cout << pow( abs(fftc[0]), 2);
 
   // std::vector<double> pSpectrum;
-  std::vector<double> pSpectrum;
-  for(int i = 0; i<NUM_FFT/2+1; i++) {
-    pSpectrum[i] = pow(abs(fftc[i]),2);
-  }
-
-  // pre-emphasis and hamming filtered spectrum
-  plt::plot(aSpectrum);
-  plt::show();
-
+  //std::vector<double> pSpectrum;
+  //for(int i = 0; i<NUM_FFT/2+1; i++) {
+  //  pSpectrum[i] = pow(abs(fftc[i]),2);
+  //}
+  std::cout << abs(fftc[0]);
 }
 
 void preEmphasisAndHamming(std::vector<double> frame) {
@@ -74,10 +66,6 @@ void preEmphasisAndHamming(std::vector<double> frame) {
     f[i] = frame[i] - ( 0.97 * frame[i - 1]);
     frame[i] = f[i] * (0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1)));
   }
-
-  // pre-emphasis and hamming filtered spectrum
-  plt::plot(frame);
-  plt::show();
 }
 
 void process(int16_t* samples, size_t N) {
@@ -86,10 +74,6 @@ void process(int16_t* samples, size_t N) {
   for(int i = 0; i < N; i++) {
     frame.at(i) = samples[i];
   }
-
-  // // original signal
-  // plt::plot(frame);
-  // plt::show();
 
   preEmphasisAndHamming(frame);
   powerSpectrum(frame);
