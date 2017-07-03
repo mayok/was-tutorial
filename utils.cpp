f684bdac7c71ea818200a6e43e821072558eeb1c
Naohiro IWAMOTO <naodai24@gmail.com>
2017-07-03 20:41:23 +0900

    pre emphasis filter と hamming window をくっつけた

--- utils.cpp
+++ utils.cpp
@@ -12,41 +12,32 @@ extern "C" {
   }
 
 /*
- * pre emphasis filter
- *   y(n) = x(n) - p x(n - 1)
+ * pre emphasis filter and hamming window
+ *
  */
 EMSCRIPTEN_KEEPALIVE
-void preEmphasis(float * _signal, size_t length, float p) {
-  std::vector<float> y(length);
+void preEmphHamming(std::vector<float>& _signal) {
 
-  printf("signal at 200 is %f\n", _signal[200]);
-  printf("signal at 400 is %f\n", _signal[400]);
-  printf("signal at 800 is %f\n", _signal[800]);
-  printf("signal at 1600 is %f\n", _signal[1600]);
+  // 0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1) ) = 0.08 if N == 1
+  // y.at(0) = _signal[0] * 0.08;
+  _signal.at(0) *= 0.08;
 
-  y.at(0) = _signal[0];
-
-  for(int i = 1; i < length; i++) {
-    y.at(i) = _signal[i] - ( p * _signal[i - 1]);
+  for (int i = 1; i < _signal.size(); i++) {
+    _signal.at(i) -= 0.97 * _signal.at(i - 1);
+    _signal.at(i) *= 0.54 - 0.46 * cos( 2.0 * PI * i / (_signal.size() - 1) );
   }
-
-  printf("filtered signal at 200 is %f\n", y[200]);
-  printf("filtered signal at 400 is %f\n", y[400]);
-  printf("filtered signal at 800 is %f\n", y[800]);
-  printf("filtered signal at 1600 is %f\n", y[1600]);
 }
 
-/*
- * hamming window
- *   w(n) = 0.54 - 0.46 cos( 2 PI n / (M - 1) ), 0 <= n <= M - 1
- */
-// std::array<float, N> hamming(std::array<float, N> _signal) {
-//   std::array<float, N> w;
-//   for(int i = 0; i < N; i += 1) {
-//     w[i] = _signal[i] * (0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1)));
-//   }
-//
-//   return w;
-// }
+EMSCRIPTEN_KEEPALIVE
+void mfcc(float * _signal, size_t length) {
+  // TODO: もっといい感じの書き方
+  std::vector<float> s(length);
+  for(int i=0; i<length; i++) s.at(i) = _signal[i];
+
+  preEmphHamming(s);
+  // powerSpectrum(s);
+  // lmfb(s);
+  // dct(s);
+}
 
 }
