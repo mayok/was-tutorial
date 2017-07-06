ceb9c70f6820bc17e6e1ed2647db3e6ac5372097
Naohiro IWAMOTO <naodai24@gmail.com>
2017-07-06 15:22:07 +0900

    bug fix, mfcc を求めることができるようになった
    Todo:
     - fft の中で vector complex をつかう
     - melFilterBank の計算部分を元に戻す

--- utils.cpp
+++ utils.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <math.h>
+#include <cmath>
 #include <vector>
 #include <complex>
 #include <map>
 #include <algorithm>
 #include <emscripten/emscripten.h>
 
-#define PI 3.14159265
+#define PI 4*atan(1.0)
 #define NUM_FFT 2048
 
 extern "C" {
@@ -15,6 +15,7 @@ extern "C" {
     return 0;
   }
 
+  // C-linkage ... というエラーが出るので，書き直した
   // EMSCRIPTEN_KEEPALIVE
   // std::vector<std::complex<double> > fft(std::vector<std::complex<double> > x) {
   //   int N = x.size();
@@ -57,13 +58,13 @@ extern "C" {
     for(int i = 0; i < n; i++) {
       int t = 0;
       for(int j = 0, h = i, k = n; ; h >>= 1) {
-        j = (j << 1) | (h & 1);
-
         k>>=1;
         if(k == 0) {
           t = j;
           break;
         }
+
+        j = (j << 1) | (h & 1);
       }
 
       if(t > i) {
@@ -92,27 +93,10 @@ extern "C" {
   *
   */
   EMSCRIPTEN_KEEPALIVE
-  void melFilterBank(float filterbank[20][20]) {
-    // int fmax = 44100 / 2; // sampleRate / 2
-    float melmax = 1127.01048 * log( 22050 / 700.0 + 1.0);
-    int nmax = NUM_FFT / 2;
-
-    float df = 44100 / NUM_FFT;
-    float dmel = melmax / 21; // melmax / ( number of channels + 1)
-
-    float melcenters[20], fcenters[20], indexcenters[20];
-    for(int i = 0; i < 20; i++) {
-      melcenters[i] = (i+1) * dmel;
-      fcenters[i] = 700.0 * ( exp(melcenters[i] / 1127.01048) - 1);
-      indexcenters[i] = round(fcenters[i] / df);
-    }
-
-    float indexstart[20], indexstop[20];
-    indexstart[0] = 0;
-    std::copy( indexcenters, indexcenters + 19, indexstart + 1);
-    std::copy( indexcenters + 1, indexcenters + 20, indexstop);
-    indexstop[19] = nmax;
-    // TODO: ここまでは先に計算できそう
+  void melFilterBank(float filterbank[20][1024]) {
+    int indexcenters[20] = { 6, 13, 21, 31, 42, 55, 71, 90, 112, 138, 169, 205, 248, 299, 358, 429, 512, 610, 726, 863 };
+    int indexstart[20] = { 0, 6, 13, 21, 31, 42, 55, 71, 90, 112, 138, 169, 205, 248, 299, 358, 429, 512, 610, 726 };
+    int indexstop[20] = { 13, 21, 31, 42, 55, 71, 90, 112, 138, 169, 205, 248, 299, 358, 429, 512, 610, 726, 863, 1024 };
 
     for(int i = 0; i < 20; i++) {
 
@@ -124,8 +108,6 @@ extern "C" {
       for(int j = indexcenters[i]; j < indexstop[i]; j++)
         filterbank[i][j] = 1.0 - (j - indexcenters[i]) * decrement;
     }
-
-    // fcenters は plot する時に必要になる plot(x: fcenters, y: filterbank);
   }
 
   /*
@@ -134,15 +116,14 @@ extern "C" {
   */
   EMSCRIPTEN_KEEPALIVE
   void preEmphHamming(std::vector<float>& _signal) {
+    size_t len = _signal.size();
+    std::vector<float> y(len, _signal[0] * 0.08);
 
-    // 0.54 - 0.46 * cos( 2.0 * PI * i / (N - 1) ) = 0.08 if N == 1
-    // y.at(0) = _signal[0] * 0.08;
-    _signal.at(0) *= 0.08;
-
-    for (int i = 1; i < _signal.size(); i++) {
-      _signal.at(i) -= 0.97 * _signal.at(i - 1);
-      _signal.at(i) *= 0.54 - 0.46 * cos( 2.0 * PI * i / (_signal.size() - 1) );
+    for (int i = 1; i < len; i++) {
+      y[i] = _signal[i] - ( 0.97 * _signal[i-1]);
+      y[i] *= 0.54 - 0.46 * cos( 2.0 * PI * i / (len - 1) );
     }
+    _signal = y;
   }
 
   /*
@@ -151,31 +132,28 @@ extern "C" {
    */
   EMSCRIPTEN_KEEPALIVE
   void powerSpectrum(std::vector<float>& _signal) {
-    _signal.resize(NUM_FFT);
+    _signal.resize(NUM_FFT, 0);
     std::vector<float> im(_signal.size(), 0);
     fft(_signal, im);
 
    // power spectrum
    for(int i = 0; i<NUM_FFT/2+1; i++) {
-    // _signal[i] = pow(abs(fftc[i]), 2);
-    // _signal[i] = abs(fftc[i]);
     _signal[i] = _signal[i] * _signal[i] + im[i] * im[i];
 
-    // amplitude spectrum
-    // _signal[i] = sqrt(_signal[i]);
+    // XXX: we use amplitude spectrum
+    _signal[i] = sqrt(_signal[i]);
    }
   }
 
   EMSCRIPTEN_KEEPALIVE
-  void lmfb(std::vector<float>& _signal, float mspec[20]) {
-    float filterbank[20][20];
+  void lmfb(std::vector<float>& _signal, std::vector<float>& mspec) {
+    float filterbank[20][1024];
     melFilterBank(filterbank);
 
-    //  std::vector<float> mspec(20);
     for(int i = 0; i < 20; i++) {
       float t = 0.0;
-      for(int j = 0; j < 20; j++) {
-        t += _signal[i] * filterbank[i][j];
+      for(int j = 0; j < 1024; j++) {
+        t += _signal[j] * filterbank[i][j];
       }
       mspec[i] = log10( t );
     }
@@ -189,19 +167,17 @@ extern "C" {
    *            n=0
    */
   EMSCRIPTEN_KEEPALIVE
-  void dct(float mspec[20]) {
-    float y[20];
+  void dct(std::vector<float>& mspec) {
+    std::vector<float> y(20);
     for(int k = 0; k < 20; k++) {
-      if(k == 0)
-        float t = sqrt(1 / 80);
-      float t = sqrt(1 / 40);
-
       float s = 0.0;
       for(int n = 0; n < 20; n++) {
         s += mspec[n] * cos( PI * k * ( 2 * n + 1 ) / 40.0);
       }
-
-      y[k] = t * 2 * s;
+      if(k == 0)
+        y[k] = sqrt(0.0125) * 2 * s;
+      else
+        y[k] = sqrt(0.025) * 2 * s;
     }
     mspec = y;
   }
@@ -209,7 +185,7 @@ extern "C" {
 
   EMSCRIPTEN_KEEPALIVE
   void mfcc(float * _signal, size_t length) {
-    float mspec[20];
+    std::vector<float> mspec(20);
 
     // TODO: 配列から vector に代入する, もっといい感じの書き方
     std::vector<float> s(length);
